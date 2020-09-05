5e7c33327710437eb9235cd88473414c6f8b48b7
borielvi <borielvi@isu.edu>
2020-09-04 20:54:29 -0700

    created server

---
+++ server.js
@@ -0,0 +1,27 @@
+// module imports
+const express = require('express');
+// const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+
+// create server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// session data
+// placeholder ...................
+
+
+// server middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// turn on routes
+// app.use(routes);
+
+
+// connect to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port ' + `${PORT}`));
+});
