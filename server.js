42d7e6951faedf4475f3dd4d80ac98ae7541023d
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-05 04:28:44 -0400

    added session data/middleware

--- server.js
+++ server.js
@@ -3,20 +3,30 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+
+// session data
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// placeholder ...................
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 // create server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-// session data
-// placeholder ...................
-
-
 // server middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
