396836460e00babc48e2c0f2470158981e5e2f61
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-10 13:55:11 -0400

    started on admin routes

---
+++ controllers/admin-routes.js
@@ -0,0 +1,11 @@
+const router = require('express');
+
+const { Project } = require('../models');
+const {authUser, authRole } = require('../utils/auth');
+
+///get all projects
+///get all issues
+//update user role
+//delete user
+
+module.exports = router;
