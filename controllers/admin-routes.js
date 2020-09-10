37cd572d6b4f52de3a0064379e1f711f056fa9e6
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-10 14:09:13 -0400

    started on get all projects route

--- controllers/admin-routes.js
+++ controllers/admin-routes.js
@@ -1,13 +1,43 @@
 const router = require('express');
 
-const { Project } = require('../models');
+const { User, Project } = require('../models');
 const {authUser, authRole } = require('../utils/auth');
 
 ///get all projects
+router.length('/projects', authUser, authRole, (req, res) => {
+    Project.findAll({
+
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 ///get all issues
 //update user role
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-//delete a user ---only admins should be able to delete users
+
+//delete a user ---only admins should be able to delete other users
 router.delete('/:id', authUser, authRole(user_role.ADMIN), (req, res) => {
     User.destroy({
         where: {
