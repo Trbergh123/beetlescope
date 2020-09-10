fd4031edd005cbdfd208d8072a363816d83ca2fa
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-10 14:01:33 -0400

    moved delete route from user-routes to admin-routes since only admins can remove users

--- controllers/admin-routes.js
+++ controllers/admin-routes.js
@@ -6,6 +6,26 @@ const {authUser, authRole } = require('../utils/auth');
 ///get all projects
 ///get all issues
 //update user role
-//delete user
+
+//delete a user ---only admins should be able to delete users
+router.delete('/:id', authUser, authRole(user_role.ADMIN), (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,25 +102,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-//delete a user ---only admins should be able to delete users
-router.delete('/:id', authUser, authRole(user_role.ADMIN), (req, res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
 
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
