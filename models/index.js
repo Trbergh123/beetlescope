72ee8a6f2f82e23665401e6f0f80367ca5729b4e
borielvi <65315849+borielvi@users.noreply.github.com>
2020-09-12 17:30:42 -0700

    fixed possible route error

--- models/index.js
+++ models/index.js
@@ -28,4 +28,20 @@ Task.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+Task.hasMany(Comment, {
+    foreignKey: 'task_id'
+});
+
+Comment.belongsTo(Task, {
+    foreignKey: 'task_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Project, Task, Comment };
