b0ad582b5c8290aa8132b2724af98e1cd047c2bc
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-12 20:11:18 -0400

    added admin.handlebars

--- controllers/admin-routes.js
+++ controllers/admin-routes.js
@@ -2,11 +2,10 @@ const router = require('express').Rtouter();
 const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 const {authUser, authRole } = require('../utils/auth');
-
-///get all projects
-router.length('/projects', authUser, authRole, (req, res) => {
-    Project.findAll({
-
+//get all users
+router.get('/', authUser, (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -14,6 +13,27 @@ router.length('/projects', authUser, authRole, (req, res) => {
         res.status(500).json(err);
     });
 });
+//get user by id ---should also be logged in
+router.get('/:id', authUser, (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 ///get all issues
 //update user role
 router.put('/:id', (req, res) => {
