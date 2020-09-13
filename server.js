fa251dd8ef8ef5b235697643c46e9390996e448c
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-12 21:32:21 -0400

    updated server.js to match develop branch

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-  // module imports
+// module imports
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 const session = require('express-session');
+
 
 // session data
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 // placeholder ...................
 
 const sess = {
@@ -23,15 +27,23 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
+
 // server middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
-app.use(setUser);
+
+
 
 // turn on routes
 app.use(routes);
 
+
+ 
 // connect to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening on port ' + `${PORT}`));
