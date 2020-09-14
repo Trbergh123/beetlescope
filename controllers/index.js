06cd35bc877480400de983010c46c79e6079d55b
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-14 14:20:39 -0400

    added task templates

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,8 @@ const dashboardRoutes = require('./dashboard-routes.js');
 const adminRoutes = require('./admin-routes.js');
 const myprojectRoutes = require('./myprojectspage-route');
 const newprojectRoutes = require('./newproject-routes');
+const mytaskRoutes = require('./mytasks-routes');
+const newtaskRoutes = require('./newtask-routes');
 
 
 router.use('/', dashboardRoutes);
@@ -11,6 +13,8 @@ router.use('/api', apiRoutes);
 router.use('/admin', adminRoutes);
 router.use('/myprojects', myprojectRoutes);
 router.use('/newproject', newprojectRoutes);
+router.use('/mytasks', mytaskRoutes);
+router.use('/newtask', newtaskRoutes);
 // router.use((req, res) => {
 //     res.status(404).end();
 // });
