75e97afcefc6b575bf69c9a252e2486be40cd88f
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-14 14:07:31 -0400

    added new project template

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,14 @@ const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
 const adminRoutes = require('./admin-routes.js');
 const myprojectRoutes = require('./myprojectspage-route');
+const newprojectRoutes = require('./newproject-routes');
+
 
 router.use('/', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/admin', adminRoutes);
 router.use('/myprojects', myprojectRoutes);
+router.use('/newproject', newprojectRoutes);
 // router.use((req, res) => {
 //     res.status(404).end();
 // });
