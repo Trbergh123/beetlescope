c1aedb9f8720b37c7c640c483c41043bc28bdaa3
borielvi <borielvi@isu.edu>
2020-09-14 15:48:59 -0700

    added login.js with bugs still

---
+++ public/javascript/login.js
@@ -0,0 +1,26 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            console.log("testing");
+            //document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#login_btn').addEventListener('submit', loginFormHandler);
