6f5fda58e1bddaf8ccf1b3e65637ceebde260824
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-16 23:46:41 -0400

    added formaction to button, button now redirects to projects page, still cant get post

--- controllers/newproject-routes.js
+++ controllers/newproject-routes.js
@@ -23,8 +23,7 @@ router.post('/', (req, res) => {
     Project.create({
         title: req.body.title,
         project_text: req.body.project_text,
-        user_id: req.params.user_id,
-        users_with_access: req.params.users_with_access
+        users_with_access: req.body.users_with_access
         
 
     })
