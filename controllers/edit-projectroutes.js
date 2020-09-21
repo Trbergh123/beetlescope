6fac68c55fd46f859ce1f97c871f0103ae3ab10b
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-09-21 01:54:25 -0400

    changed up project details page. working on getting it fully mobille responsive

--- controllers/edit-projectroutes.js
+++ controllers/edit-projectroutes.js
@@ -4,73 +4,73 @@ const {authUser} = require("../utils/auth");
 const { Task } = require("../models/")
 
 
-router.get('/', (req, res) => {
-  res.render('edit-project');
-})
-router.get("/projects", authUser, (req, res) => {
-    Project.findAll({
-      where: {
-        userId: req.session.userId
-      }
-    })
-      .then(dbPostData => {
-        const projects = dbPostData.map((project) => project.get({ plain: true }));
+// router.get('/', (req, res) => {
+//   res.render('edit-project');
+// })
+// router.get("/projects", authUser, (req, res) => {
+//     Project.findAll({
+//       where: {
+//         userId: req.session.userId
+//       }
+//     })
+//       .then(dbPostData => {
+//         const projects = dbPostData.map((project) => project.get({ plain: true }));
         
-        res.render("all-posts-admin", {
-          layout: "dashboard",
-          projects
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.redirect("login"); //if not logged in, if authUser isn't true, then boom redirect to login.
-      });
-  });
+//         res.render("all-posts-admin", {
+//           layout: "dashboard",
+//           projects
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//         res.redirect("login"); //if not logged in, if authUser isn't true, then boom redirect to login.
+//       });
+//   });
 
   
 
-  router.get("/newproject", authUser, (req, res) => {
-    res.render("newproject", {
-      layout: "main"
-    });
-  });
+//   router.get("/newproject", authUser, (req, res) => {
+//     res.render("newproject", {
+//       layout: "main"
+//     });
+//   });
   
-  router.get("/edit/:id", authUser, (req, res) => {
-    Project.findByPk(req.params.id)
-      .then(dbPostData => {
-        if (dbPostData) {
-          const project = dbPostData.get({ plain: true });
+//   router.get("/edit/:id", authUser, (req, res) => {
+//     Project.findByPk(req.params.id)
+//       .then(dbPostData => {
+//         if (dbPostData) {
+//           const project = dbPostData.get({ plain: true });
           
-          res.render("edit-project", {
-            layout: "main",
-            project
-          });
-        } else {
-          res.status(404).end();
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
-  });
+//           res.render("edit-project", {
+//             layout: "main",
+//             project
+//           });
+//         } else {
+//           res.status(404).end();
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).json(err);
+//       });
+//   });
 
-  router.get('/', (req, res) => {
-    Task.findAll({
+//   router.get('/', (req, res) => {
+//     Task.findAll({
         
-        attributes: [
-            'title',
-            'type',
-            'project_id',
-            'users_with_access',
-            'created_at'
-        ]
-    })
-    .then(dbTasksData => {
-        const tasks = dbTasksData.map((tasks) => tasks.get({plain: true}));
-        res.render('edit-project', {tasks:tasks})
-    })
+//         attributes: [
+//             'title',
+//             'type',
+//             'project_id',
+//             'users_with_access',
+//             'created_at'
+//         ]
+//     })
+//     .then(dbTasksData => {
+//         const tasks = dbTasksData.map((tasks) => tasks.get({plain: true}));
+//         res.render('edit-project', {tasks:tasks})
+//     })
     
-});
+// });
 
 router.get('/api/users', (req, res) => {
    
